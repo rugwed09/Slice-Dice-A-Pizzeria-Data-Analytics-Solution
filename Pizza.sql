713246f41dc04099b9acccc7deaf2a208889f0f8
Rugwed Pimple <rugwedpimpale@gmail.com>
2024-02-17 00:28:01 -0500

    Update Pizza.sql

--- Pizza.sql
+++ Pizza.sql
@@ -1,8 +1,3 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/rAoX3C
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
 CREATE TABLE `orders` (
     `row_id` int  NOT NULL ,
     `order_id` varchar(10)  NOT NULL ,
